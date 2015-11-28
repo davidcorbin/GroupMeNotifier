fa1b09e87a76ac3bc6bbd0a37bb3df7541e3b319
David Corbin <daconex@gmail.com>
2015-11-28 14:01:35 -0500

    Added groupme.ico used for notifications and cleaned up WindowsNotification.py

--- groupmenotifier/WindowsNotification.py
+++ groupmenotifier/WindowsNotification.py
@@ -1,13 +1,13 @@
 from win32gui import *
+import os, sys
 import win32con
-import sys, os
 import time
 
 
 class WindowsNotification:
     def __init__(self, title, msg):
         message_map = {
-                win32con.WM_DESTROY: self.OnDestroy,
+            win32con.WM_DESTROY: self.OnDestroy,
         }
         # Register the Window class.
         wc = WNDCLASS()
@@ -17,24 +17,24 @@ class WindowsNotification:
         classAtom = RegisterClass(wc)
         # Create the Window.
         style = win32con.WS_OVERLAPPED | win32con.WS_SYSMENU
-        self.hwnd = CreateWindow(classAtom, "Taskbar", style, \
-                0, 0, win32con.CW_USEDEFAULT, win32con.CW_USEDEFAULT, \
+        self.hwnd = CreateWindow(classAtom, "Taskbar", style,
+                0, 0, win32con.CW_USEDEFAULT, win32con.CW_USEDEFAULT,
                 0, 0, hinst, None)
         UpdateWindow(self.hwnd)
-        #iconPathName = os.path.abspath(os.path.join( sys.path[0], "balloontip.ico" ))
-        iconPathName = "groupme3.png"
+        iconpath = os.path.abspath(os.path.join(sys.path[0], "groupme.ico"))
         icon_flags = win32con.LR_LOADFROMFILE | win32con.LR_DEFAULTSIZE
         try:
-           hicon = LoadImage(hinst, iconPathName, \
-                    win32con.IMAGE_ICON, 0, 0, icon_flags)
+            hicon = LoadImage(hinst, iconpath, win32con.IMAGE_ICON, 0, 0, icon_flags)
         except:
-          hicon = LoadIcon(0, win32con.IDI_APPLICATION)
+            hicon = LoadIcon(0, win32con.IDI_APPLICATION)
+
+        # Taskbar icon
         flags = NIF_ICON | NIF_MESSAGE | NIF_TIP
-        nid = (self.hwnd, 0, flags, win32con.WM_USER+20, hicon, "tooltip")
+        nid = (self.hwnd, 0, flags, win32con.WM_USER+20, hicon, "Tooltip")
         Shell_NotifyIcon(NIM_ADD, nid)
-        Shell_NotifyIcon(NIM_MODIFY, \
-                         (self.hwnd, 0, NIF_INFO, win32con.WM_USER+20,\
-                          hicon, "Balloon  tooltip",msg,200,title))
+        Shell_NotifyIcon(NIM_MODIFY,
+                         (self.hwnd, 0, NIF_INFO, win32con.WM_USER+20,
+                          hicon, "Balloon  tooltip", msg, 200, title))
         # self.show_balloon(title, msg)
         time.sleep(10)
         DestroyWindow(self.hwnd)
@@ -42,8 +42,8 @@ class WindowsNotification:
     def OnDestroy(self, hwnd, msg, wparam, lparam):
         nid = (self.hwnd, 0)
         Shell_NotifyIcon(NIM_DELETE, nid)
-        PostQuitMessage(0) # Terminate the app.
+        PostQuitMessage(0)  # Terminate the app.
 
 
 def notify(title, msg):
-    w = WindowsNotification(title, msg)
+    WindowsNotification(title, msg)
