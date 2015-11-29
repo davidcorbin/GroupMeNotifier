d555230e1b206118815d578f2c06c30ec28dc753
David Corbin <daconex@gmail.com>
2015-11-29 16:04:18 -0500

    Added 302 redirect to daconex.me

--- groupmenotifier/groupme.py
+++ groupmenotifier/groupme.py
@@ -23,10 +23,13 @@ class GroupMe:
     requestid = 1
     cacert = "cacert.pem"
 
+    # Constructor
     def __init__(self):
-        print "New Instance of GroupMe"
+        print "New Instance of GroupMe notifier"
 
+    #
     # Method for connecting to GroupMe
+    #
     def handshake(self):
         # Exact handshake string
         data = """
@@ -52,7 +55,9 @@ class GroupMe:
             self.clientId = p[0]["clientId"]
             print "Handshake complete\n"
 
+    #
     # Method to get API access token from GroupMe
+    #
     def auth(self):
         # If access_token file doesn't exist or is empty
         if not os.path.isfile(self.access_token_dir) or os.stat(self.access_token_dir).st_size == 0:
@@ -60,8 +65,10 @@ class GroupMe:
             print "access_token file not found"
             webbrowser.open(self.request_URL)
             while self.access_token is "":
-                time.sleep(5)
-            self.shutdown_server()
+                time.sleep(1)
+            # print "Shutting down server..."
+            # self.shutdown_server()
+            # print "Server shut down"
             # If access token ends with a forward slash
             if self.access_token.endswith("/"):
                 self.access_token = self.access_token[:-1]
@@ -69,11 +76,14 @@ class GroupMe:
             token_file = open(self.access_token_dir, "w")
             token_file.write(self.access_token)
             token_file.close()
+            print "token_access file written"
 
         token_file = open(self.access_token_dir, "r")
         self.access_token = token_file.read()
 
+    #
     # Method for subscribing to groups
+    #
     def subscribe(self, groupid):
 
         data = """
@@ -97,7 +107,9 @@ class GroupMe:
         print r.text
         self.increment_request_id()
 
+    #
     # Method for connecting to GroupMe API servers and waiting for messages
+    #
     def connect(self):
         ws = websocket.create_connection("wss://push.groupme.com/faye",
                     sslopt={"check_hostname": False, "cert_reqs": ssl.CERT_NONE, "ca_certs": "cacert.pem"})
@@ -125,7 +137,9 @@ class GroupMe:
                 notify("GroupMe", r[0]["data"]["alert"])
             print r
 
+    #
     # Method for getting a list if group id's
+    #
     def groups(self):
         group_list = []
         url = "https://api.groupme.com/v3/groups?per_page=500&access_token="+self.access_token
@@ -139,7 +153,9 @@ class GroupMe:
         print "List of groups: %s\n" % group_list
         return group_list
 
+    #
     # Method to subscribe to messages from all my channels
+    #
     def user_subscribe(self):
         data = """
     [
@@ -164,7 +180,9 @@ class GroupMe:
         # p = json.loads(r.text)
         print r.text
 
+    #
     # Method to get user details
+    #
     def user_details(self):
         url = "https://api.groupme.com/v3/users/me?access_token="+self.access_token
         print "Getting user info"
@@ -174,11 +192,15 @@ class GroupMe:
         self.userid = p["response"]["id"]
         print "Successfully got user details"
 
+    #
     # Method to increase the request id for every request to GroupMe
+    #
     def increment_request_id(self):
         self.requestid += 1
 
+    #
     # Method to start server to capture access token
+    #
     def startserver(self):
         print "Server started"
         handler = GetHandler
@@ -187,13 +209,22 @@ class GroupMe:
         thread.deamon = True
         thread.start()
 
+    #
     # Method to shutdown SimpleHTTPServer after access token received
+    #
     def shutdown_server(self):
         self.server.shutdown()
 
 
+# Handler extending SimpleHTTPRequestHandler to capture localhost:8000 GET requests when server is started
 class GetHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
         print "GET request captured: " + self.path
         if self.path.startswith('/?access_token'):
+            print "Access token received"
             GroupMe.access_token = self.path.split("=", 1)[1]
+            self.send_response(302)
+            self.send_header("Location", "http://daconex.me")
+            self.end_headers()
+        else:
+            self.do_GET()
