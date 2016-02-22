bba9689314d81bf9e642037730610d2599a20f49
David Corbin <daconex@gmail.com>
2016-02-22 14:41:51 -0500

    Added travis ci file

--- setup.py
+++ setup.py
@@ -1,27 +1,15 @@
-from cx_Freeze import setup, Executable
-import requests.certs
-import sys
-import os
-
-# Added groupmenotifier/ directory to system path so thay cxfreeze includes groupme.py, etc.
-sys.path.append(os.getcwd()+"\\groupmenotifier")
-
-options = {
-    'build_exe': {
-        'includes': ["groupme"],
-        'include_files': [(requests.certs.where(), 'cacert.pem')]
-    }
-}
+from setuptools import setup
 
 setup(
     name='GroupMe Notifier',
-    version='0.1',
-    url='http://daconex.me',
-    license='MIT',
+    version=0.1,
+    description='GroupMe Notifier is a tool for getting notifications on desktop',
     author='David Corbin',
     author_email='daconex+groupmenotifier@gmail.com',
-    description='Windows 10 client for GroupMe',
-    options=options,
-    executables=[Executable("groupmenotifier/main.py")],
-    requires=['requests', 'cx_Freeze']
+    url='http://daconex.me',
+    install_requires=['requests']
 )
+
+
+
+
